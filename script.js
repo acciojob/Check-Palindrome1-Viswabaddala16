a9bc2cf4ed09104a1fe83f2741f586b79922342b
Viswabaddala16 <122138862+Viswabaddala16@users.noreply.github.com>
2023-11-17 09:04:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 // complete the given function
 
-function palindrome(str){
+ function palindrome(str) {
+    // Function to check if a character is alphanumeric
+    function isAlphanumeric(char) {
+        const code = char.charCodeAt(0);
+        return (code >= 48 && code <= 57) || (code >= 65 && code <= 90) || (code >= 97 && code <= 122);
+    }
 
-}
+    // Function to convert a character to lowercase
+    function toLowerCase(char) {
+        return char.toLowerCase();
+    }
+
+    // Clean and lowercase the string
+    let cleanStr = '';
+    for (let i = 0; i < str.length; i++) {
+        if (isAlphanumeric(str[i])) {
+            cleanStr += toLowerCase(str[i]);
+        }
+    }
+
+    // Check if the cleaned string is a palindrome
+    const length = cleanStr.length;
+    for (let i = 0; i < Math.floor(length / 2); i++) {
+        if (cleanStr[i] !== cleanStr[length - 1 - i]) {
+            return false;
+        }
+    }
+
+    return true;
+ }
 module.exports = palindrome
